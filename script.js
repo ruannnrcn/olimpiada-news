77c908c2b87397dc65d58da4982ef4cf2100c8e3
Pedro Ruan <pedro.ruan251202@gmail.com>
2023-01-26 11:51:44 -0300

    adicionar darkmode e mudança de cores

---
+++ script.js
@@ -0,0 +1,19 @@
+// let entrada = parseInt(prompt("Digite o ano que você nasceu"));
+// alert(2023 + entrada);
+// alert(typeof entrada);
+//ctrl + ;
+
+let main = document.querySelector("main");
+
+function toDark() {
+  main.classList.add("dark");
+  main.classList.remove("light");
+}
+
+function toLight() {
+  main.classList.add("light");
+  main.classList.remove("dark");
+}
+
+toDark;
+toLight;
